2d514fbef704459aaf407ec6259e817d30fdff8f
Lewis Kathembe <lewiskathembe51@gmail.com>
2025-01-11 18:41:19 +0300

    Initial setup for BookInventory-DevOps

---
+++ backend/src/main/java/com/bookinventory/controller/BookController.java
@@ -0,0 +1,22 @@
+package com.bookinventory.controller;
+
+import com.bookinventory.model.Book;
+import com.bookinventory.repository.BookRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BookController {
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping("/books")
+    public List<Book> getBooks() {
+        return bookRepository.findAll();
+    }
+}
